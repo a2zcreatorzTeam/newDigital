c51b71ab9871dde848e273538b38ba582b1bc0ee
M Shoaib nasir <95880880+MShoaibNasir@users.noreply.github.com>
2026-04-17 17:58:38 +0500

    working on middleware

--- app/Http/Middleware/CheckUserRole.php
+++ app/Http/Middleware/CheckUserRole.php
@@ -5,7 +5,7 @@ namespace App\Http\Middleware;
 use Closure;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
-
+use Auth;
 class CheckUserRole
 {
     /**
@@ -13,8 +13,16 @@ class CheckUserRole
      *
      * @param  Closure(Request): (Response)  $next
      */
-    public function handle(Request $request, Closure $next): Response
+    public function handle($request, Closure $next)
     {
-        return $next($request);
+        if (Auth::check()) {
+            if (Auth::user()->user_type == 2) {
+               return $next($request);
+            }else{
+                return redirect()->route('admin.login');
+            }
+        }
+
+        return redirect()->route('admin.login');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,7 @@ use App\Http\Controllers\backend\SubClassController;
 
 
 Route::get('/', function () {
-    return view('welcome');
+        return view('welcome');
 });
 
 Auth::routes();
@@ -19,9 +19,32 @@ Auth::routes();
 
 Auth::routes();
 
-Route::group(['middleware' => ['auth']], function () {
-    Route::resource('roles', RoleController::class);
-    Route::resource('users', UserController::class);
+Route::group(['middleware' => ['user.role']], function () {
+        Route::resource('roles', RoleController::class);
+        Route::resource('users', UserController::class);
+        Route::prefix('admin/dashboard')->name('admin.')->controller(DashboardController::class)->group(function () {
+                Route::get('/', 'dashboard')->name('dashboard');
+        });
+        Route::prefix('admin/dashboard/class')->name('class.')->controller(MainClassController::class)->group(function () {
+                Route::get('/index', 'index')->name('index');
+                Route::get('/create', 'create')->name('create');
+                Route::post('/store', 'store')->name('store');
+                Route::get('/edit/{id}', 'edit')->name('edit');
+                Route::put('/update/{id}', 'update')->name('update');
+                Route::delete('/destroy/{id}', 'destroy')->name('destroy');
+                Route::get('/toggleStatus/{id}', 'toggleStatus')->name('toggleStatus');
+        });
+        Route::prefix('admin/dashboard/subclass')->name('subclass.')->controller(SubClassController::class)->group(function () {
+                Route::get('/index', 'index')->name('index');
+                Route::post('/list', 'list')->name('list');
+                Route::get('/create', 'create')->name('create');
+                Route::get('/filter', 'filter')->name('filter');
+                Route::post('/store', 'store')->name('store');
+                Route::get('/edit/{id}', 'edit')->name('edit');
+                Route::put('/update/{id}', 'update')->name('update');
+                Route::delete('/destroy/{id}', 'destroy')->name('destroy');
+                Route::get('/toggleStatus/{id}', 'toggleStatus')->name('toggleStatus');
+        });
 });
 
 
@@ -29,28 +52,4 @@ Route::prefix('admin')->name('admin.')->controller(AuthController::class)->group
         Route::get('login/', 'loginPage')->name('login');
         Route::post('user/login', 'login')->name('login.user');
         Route::get('user/logout', 'logout')->name('logout.user');
-
-});
-Route::prefix('admin/dashboard')->name('admin.')->controller(DashboardController::class)->group(function () {
-        Route::get('/', 'dashboard')->name('dashboard');
-});
-Route::prefix('admin/dashboard/class')->name('class.')->controller(MainClassController::class)->group(function () {
-        Route::get('/index', 'index')->name('index');
-        Route::get('/create', 'create')->name('create');
-        Route::post('/store', 'store')->name('store');
-        Route::get('/edit/{id}', 'edit')->name('edit');
-        Route::put('/update/{id}', 'update')->name('update');
-        Route::delete('/destroy/{id}', 'destroy')->name('destroy');
-        Route::get('/toggleStatus/{id}', 'toggleStatus')->name('toggleStatus');
-});
-Route::prefix('admin/dashboard/subclass')->name('subclass.')->controller(SubClassController::class)->group(function () {
-        Route::get('/index', 'index')->name('index');
-        Route::post('/list', 'list')->name('list');
-        Route::get('/create', 'create')->name('create');
-        Route::get('/filter', 'filter')->name('filter');
-        Route::post('/store', 'store')->name('store');
-        Route::get('/edit/{id}', 'edit')->name('edit');
-        Route::put('/update/{id}', 'update')->name('update');
-        Route::delete('/destroy/{id}', 'destroy')->name('destroy');
-        Route::get('/toggleStatus/{id}', 'toggleStatus')->name('toggleStatus');
 });
